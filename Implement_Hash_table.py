c238874cc06ee19f34117ed8d269a26a5e9bea7c
Larmeal <armloveyou252@hotmail.com>
2022-05-02 18:17:44 +0700

    .

--- Implement_Hash_table.py
+++ Implement_Hash_table.py
@@ -17,16 +17,15 @@ class HashTable:
     def keys(self):
         for key in self.dict:
             self.keys_list.append(key)
-        return self.keys_list 
+        return self.keys_list
     
     def values(self):
         for value in self.dict:
             self.values_list.append(self.dict[value])
         return self.values_list
-        
+
 data_1 = HashTable()
 print(data_1.set("game", 4))
 print(data_1.set("day", 2))
 print(data_1.keys())
 print(data_1.values())
-        
